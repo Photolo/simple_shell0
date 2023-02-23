278321768e28e9099a5289a648fb92ff73d81902
Ayanda16 <117756881+Ayanda16@users.noreply.github.com>
2023-02-23 02:40:48 +0200

    Update environ.c

--- environ.c
+++ environ.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _myenv - builtin to print the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
@@ -14,7 +14,7 @@ int _myenv(info_t *info)
 
 /**
  * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments.
  * @name: env var name
  *
  * Return: the value
